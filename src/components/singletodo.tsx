7462302e69d4f6e136836f7325195237f959e2f7
meetkonkani <66130472+meetkonkani@users.noreply.github.com>
2024-06-03 22:19:46 +0530

    Update singletodo.tsx

--- src/components/singletodo.tsx
+++ src/components/singletodo.tsx
@@ -1,70 +1,78 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import { Todo } from '../model';
-import { MdEdit, MdDelete, MdOutlineDone } from "react-icons/md";
-import './styles.css'
-import Todolist from './Todolist';
+import { MdEdit, MdDelete, MdOutlineDone } from 'react-icons/md';
+import './styles.css';
 
-type Props ={
-    todo:Todo,
-    todos:Todo[],
-    setTodos:React.Dispatch<React.SetStateAction<Todo[]>>;
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
 
-}
-const Singletodo = ({todo,todos,setTodos}:Props) => {
+const Singletodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
   const [edit, setEdit] = useState<boolean>(false);
-  const [ediTodo, setEditTodo] = useState<string>(todo.todo);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
 
-  const handleDelete=(id:number)=>{
-    setTodos(todos.filter((todo)=>todo.id!==id))
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
-  const handleDone=(id:number) => {setTodos(
-        todos.map((todo)=>
-          todo.id===id?{...todo,isDone:!todo.isDone}:todo)
-      );
-        setEdit(false);
-      }; 
-      
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+    setEdit(false);
+  };
+
   const inputRef = useRef<HTMLInputElement>(null);
-      useEffect(() => {
-        inputRef.current?.focus();
-      }, [edit]);
-      
-
-  const handleEdit = (e: React.FormEvent, id:number) => {
-      e.preventDefault();
-
-      setTodos(todos.map((todo) => (
-        todo.id===id?{...todo,todo:ediTodo}:todo
-      )))
+  useEffect(() => {
+    if (edit) {
+      inputRef.current?.focus();
     }
+  }, [edit]);
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo)));
+  };
+
   return (
-    <form className="todos_single" onSubmit={(e)=> handleEdit(e,todo.id)}>
-        {edit? (
-            <input value={ediTodo} onChange={(e) => setEditTodo(e.target.value)} className='todos_single--text'/> 
-          ): todo.isDone?(
-                <s className="todos_single--text">{todo.todo}</s>
-              ): (
-                <span className="todos_single--text">{todo.todo}</span>
-              )}
+    <form className="todos_single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todos_single--text"
+          ref={inputRef}
+        />
+      ) : todo.isDone ? (
+        <s className="todos_single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos_single--text">{todo.todo}</span>
+      )}
 
       <div>
-        <span className="icon" onClick={()=>{
-          if(!edit && !todo.isDone){
-            setEdit(!edit)
-          }
-        }}>
-        <MdEdit />
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
+          <MdEdit />
         </span>
-        <span className="icon" onClick={()=>handleDelete(todo.id)}>
-        <MdDelete />
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <MdDelete />
         </span>
-        <span className="icon" onClick={()=>handleDone(todo.id)}>
-        <MdOutlineDone />
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdOutlineDone />
         </span>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default Singletodo
+export default Singletodo;
